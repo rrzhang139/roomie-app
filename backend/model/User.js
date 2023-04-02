15d3d5beeada0d19a32483bbb90ceee660d73a8b
Richard Zhang <rrzhang@usc.edu>
2023-04-01 17:28:08 -0700

    init

---
+++ backend/model/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  // roommate_groups: [{ type: mongoose.Schema.Types.ObjectId, ref: 'RoommateGroup' }]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User; 
